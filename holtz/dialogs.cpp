3c1273b25e46ab87bf8643e55cef594cdd5d3ebf
rrobek <florianfischer@gmx.de>
2003-03-01 17:13:07 +0000

    adding initial version of help, modified program to show help

--- holtz/dialogs.cpp
+++ holtz/dialogs.cpp
@@ -22,15 +22,22 @@ namespace holtz
 {
   Player_Setup_Dialog::Player_Setup_Dialog( wxWindow *parent, Game_Window &game_window, 
 					    Player_Setup_Manager &player_setup_manager )
-    : wxDialog(parent,-1,wxString(_("Player setup"))),
+    : wxDialog(),
       game_window(game_window),
       player_setup_manager(&player_setup_manager),
-      player_name( new wxTextCtrl( this, -1, _("Player 1") ) ),
-      ai( new wxCheckBox( this, -1, _("AI") ) ),
-      player_list( new wxListBox( this, LISTBOX_DCLICK, wxDefaultPosition, 
-				  wxSize(200,150), 0, 0, wxLB_SINGLE ) ),
       current_ruleset(0), last_ruleset(0)
   {
+	// create the dialog
+#ifdef __WXMSW__ // this has to be done before the dialog is created, ...
+	SetExtraStyle(wxDIALOG_EX_CONTEXTHELP);
+#endif // ... hence the two-step construction (Create() below) is necessary.
+	wxDialog::Create(parent,-1,wxString(_("Player setup")));
+
+	// create the child controls
+    player_name = new wxTextCtrl( this, -1, _("Player 1") );
+    ai = new wxCheckBox( this, -1, _("AI") );
+    player_list = new wxListBox( this, LISTBOX_DCLICK, wxDefaultPosition, 
+				  wxSize(200,150), 0, 0, wxLB_SINGLE );
     // update choice names with correct translation
     wxString help_choices[3], ruleset_choices[3];
     help_choices[0] = wxString(_("No help"));
@@ -73,14 +80,29 @@ namespace holtz
     top_sizer->Add( ruleset_choice, 0, wxALIGN_CENTER );
 
     wxBoxSizer *button_sizer  = new wxBoxSizer( wxHORIZONTAL );
-    button_sizer->Add( new wxButton(this, DIALOG_READY,  _("Ready"), wxDefaultPosition) , 0, wxALL, 30 );
-    button_sizer->Add( new wxButton(this, DIALOG_CANCEL, _("Cancel"), wxDefaultPosition) , 0, wxALL, 30 );
+    button_sizer->Add( new wxButton(this, DIALOG_READY,  _("Ready"), wxDefaultPosition) , 0, wxALL, 10 );
+    button_sizer->Add( new wxButton(this, DIALOG_CANCEL, _("Cancel"), wxDefaultPosition) , 0, wxALL, 10 );
     top_sizer->Add( button_sizer, 0, wxALIGN_CENTER );
       
     SetAutoLayout( true );     // tell dialog to use sizer
     SetSizer( top_sizer );      // actually set the sizer
     top_sizer->Fit( this );            // set size to minimum size as calculated by the sizer
     top_sizer->SetSizeHints( this );   // set size hints to honour mininum size     
+
+	// set help texts
+#ifndef __WXMSW__
+    button_sizer->Add(new wxContextHelpButton(this), 0, wxALIGN_CENTER | wxALL, 10);
+#endif
+	player_name->SetHelpText(_("Type the new player's name here."));
+	player_list->SetHelpText(_("Shows all players which will be part of the new game."));
+	help_choice->SetHelpText(_("Select what kind of help you want for the new player."));
+	ruleset_choice->SetHelpText(_("Select the ruleset for the new game."));
+	add_button->SetHelpText(_("Add the new player to the game."));
+	ai->SetHelpText(_("Check this box if the new player should be computer-controlled, uncheck if it is a human player."));
+	FindWindow(DIALOG_PLAYER_UP)->SetHelpText(_("Move the currently selected player upwards. (The upmost player begins the game.)"));
+	FindWindow(DIALOG_PLAYER_DOWN)->SetHelpText(_("Move the currently selected player downwards. (The upmost player begins the game.)"));
+	FindWindow(DIALOG_REMOVE_PLAYER)->SetHelpText(_("Remove the currently selected player from the new game."));
+	FindWindow(DIALOG_READY)->SetHelpText(_("Click this button as soon as you are happy with the settings. \nNetwork games start when all players have clicked 'Ready'."));
   }
 
   Player_Setup_Dialog::~Player_Setup_Dialog()
@@ -372,24 +394,42 @@ namespace holtz
   END_EVENT_TABLE()								//**/
 
   Network_Clients_Dialog::Network_Clients_Dialog( wxWindow *parent, Network_Manager &network_manager )
-    : wxDialog(parent,-1,wxString(_("Network client setup"))),
-      client_list( new wxListBox( this, LISTBOX_DCLICK, wxDefaultPosition, 
-				  wxSize(120,120), 0, 0, wxLB_SINGLE ) ),
+    : wxDialog(),
       network_manager(network_manager)
   {
+	// create the dialog
+#ifdef __WXMSW__ // this has to be done before the dialog is created, ...
+	SetExtraStyle(wxDIALOG_EX_CONTEXTHELP);
+#endif // ... hence the two-step construction (Create() below) is necessary.
+	wxDialog::Create(parent,-1,wxString(_("Network client setup")));
+
+	// create the child controls
+    client_list = new wxListBox( this, LISTBOX_DCLICK, wxDefaultPosition, 
+				  wxSize(120,120), 0, 0, wxLB_SINGLE );
+
     network_manager.set_connection_handler(this);
 
     wxBoxSizer *top_sizer = new wxBoxSizer( wxVERTICAL );
     
     top_sizer->Add(client_list, 0, wxALL, 10);
 
-    top_sizer->Add( new wxButton( this, DIALOG_DISCONNECT, _("Disconnect All") ), 0, wxALL, 10 );
+    wxBoxSizer* button_sizer = new wxBoxSizer(wxHORIZONTAL);
+	top_sizer->Add(button_sizer, 0, wxALL, 0);
+
+	button_sizer->Add( new wxButton( this, DIALOG_DISCONNECT, _("Disconnect All") ), 0, wxALL, 10 );
     
     SetAutoLayout( true );     // tell dialog to use sizer
     SetSizer( top_sizer );      // actually set the sizer
     top_sizer->Fit( this );            // set size to minimum size as calculated by the sizer
     top_sizer->SetSizeHints( this );   // set size hints to honour mininum size     
-  }
+
+	// set help texts
+#ifndef __WXMSW__
+    button_sizer->Add(new wxContextHelpButton(this), 0, wxALIGN_CENTER | wxALL, 10);
+#endif
+	client_list->SetHelpText(_("Lists all connected client computers. Double-click one of the clients to throw him out of the game."));
+	FindWindow(DIALOG_DISCONNECT)->SetHelpText(_("Disconnect all client computers from the game."));
+}
 
   Network_Clients_Dialog::~Network_Clients_Dialog()
   {
@@ -444,12 +484,20 @@ namespace holtz
   END_EVENT_TABLE()							//**/
 
   Network_Connection_Dialog::Network_Connection_Dialog( wxWindow *parent )
-    : wxDialog(parent,-1, wxString(_("Network Game"))),
-      server( new wxRadioButton(this, -1, _("Setup server") ) ),
-      client( new wxRadioButton(this, -1, _("Connect to server") ) ),
-      hostname( new wxTextCtrl(this, -1, wxT("localhost")) ),
-      port( new wxSpinCtrl(this, -1, wxT("6211")) )
+    : wxDialog()
   {      
+	// create the dialog
+#ifdef __WXMSW__ // this has to be done before the dialog is created, ...
+	SetExtraStyle(wxDIALOG_EX_CONTEXTHELP);
+#endif // ... hence the two-step construction (Create() below) is necessary.
+	wxDialog::Create(parent,-1,wxString(_("Network Game")));
+
+	// create the child controls
+    server = new wxRadioButton(this, -1, _("Setup server") );
+    client = new wxRadioButton(this, -1, _("Connect to server") );
+    hostname = new wxTextCtrl(this, -1, wxT("localhost"));
+    port = new wxSpinCtrl(this, -1, wxT("6211"));
+
     wxBoxSizer *top_sizer = new wxBoxSizer( wxVERTICAL );
     server->SetValue(true);
     top_sizer->Add( server, 0, wxALL, 10 );
@@ -470,6 +518,16 @@ namespace holtz
     SetSizer( top_sizer );      // actually set the sizer
     top_sizer->Fit( this );            // set size to minimum size as calculated by the sizer
     top_sizer->SetSizeHints( this );   // set size hints to honour mininum size     
+
+	// set help texts
+#ifndef __WXMSW__
+    button_sizer->Add(new wxContextHelpButton(this), 0, wxALIGN_CENTER | wxALL, 10);
+#endif
+	server->SetHelpText(_("Select this option to setup a new server, so that other players on the network can join your game."));
+	client->SetHelpText(_("Select this option to connect to someone else's server and join his game."));
+	hostname->SetHelpText(_("Enter the IP address or hostname of the computer you want to connect to. (If you are setting up a server, this field has no meaning.)"));
+	port->SetHelpText(_("Enter the port number of your (or the other person's) server. You can usually leave this unchanged."));
+	FindWindow(DIALOG_OK)->SetHelpText(_("Start up the network connection using the given options."));
   }
 
   BEGIN_EVENT_TABLE(Network_Connection_Dialog, wxDialog)		//**/
