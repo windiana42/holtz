ffe6f85aa6ca27f5b9041890b3c46f05181abffe
Windiana42 <windiana@users.sf.net>
2003-03-17 22:02:13 +0000

    add links

---
+++ htdocs/links.php
@@ -0,0 +1,22 @@
+<?php require("menu.inc.php"); 
+  StartPage("Links", "Links");
+?>
+
+  <ul>
+    <li>
+      <a href="http://www.gamerz.net/pbmserv/">http://www.gamerz.net/pbmserv/</a>
+    </li>
+    <li>
+      <a href="http://groups.yahoo.com/group/zertz">http://groups.yahoo.com/group/zertz</a>
+    </li>
+    <li>
+      <a href="http://home.t-online.de/home/320018940973/assimilate.de/ZF1/index.html">
+	http://home.t-online.de/home/320018940973/assimilate.de/ZF1/index.html</a>
+    </li>
+    <li>
+      <a href="http://www.mklabs.de/MKZertzDownload/MKZertz.zip">
+	http://www.mklabs.de/MKZertzDownload/MKZertz.zip</a>
+    </li>
+  </ul>
+    
+<?php EndPage(); ?>
