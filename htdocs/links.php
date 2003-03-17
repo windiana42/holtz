61908f5ad44bc9bbc22a2ad6fb0bc076066b274c
Windiana42 <windiana@users.sf.net>
2003-03-17 22:14:00 +0000

    links

--- htdocs/links.php
+++ htdocs/links.php
@@ -4,16 +4,22 @@
 
   <ul>
     <li>
+      Play Z&egrave;ertz via EMail: 
       <a href="http://www.gamerz.net/pbmserv/">http://www.gamerz.net/pbmserv/</a>
     </li>
     <li>
-      <a href="http://groups.yahoo.com/group/zertz">http://groups.yahoo.com/group/zertz</a>
+      A newsgroup: <a href="http://groups.yahoo.com/group/zertz">http://groups.yahoo.com/group/zertz</a>
     </li>
+  </ul>
+  <h2> Some other Zertz implementations </h2>
+  <ul>
     <li>
+      ZF1: 
       <a href="http://home.t-online.de/home/320018940973/assimilate.de/ZF1/index.html">
 	http://home.t-online.de/home/320018940973/assimilate.de/ZF1/index.html</a>
     </li>
     <li>
+      MKZertz: 
       <a href="http://www.mklabs.de/MKZertzDownload/MKZertz.zip">
 	http://www.mklabs.de/MKZertzDownload/MKZertz.zip</a>
     </li>
