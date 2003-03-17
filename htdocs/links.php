28353a9e91c7a742c0e225e73baad351613d436e
Windiana42 <windiana@users.sf.net>
2003-03-17 22:19:16 +0000

    target blank

--- htdocs/links.php
+++ htdocs/links.php
@@ -6,26 +6,30 @@
   <ul>
     <li>
       Official Z&egrave;rtz Homepage: 
-      <a href="http://www.gipf.com/zertz/index.html">http://www.gipf.com/zertz/index.html</a>
+      <a href="http://www.gipf.com/zertz/index.html" target="_blank">
+	http://www.gipf.com/zertz/index.html</a>
     </li>
     <li>
       Play Z&egrave;rtz via EMail: 
-      <a href="http://www.gamerz.net/pbmserv/">http://www.gamerz.net/pbmserv/</a>
+      <a href="http://www.gamerz.net/pbmserv/" target="_blank">
+	http://www.gamerz.net/pbmserv/</a>
     </li>
     <li>
-      A newsgroup: <a href="http://groups.yahoo.com/group/zertz">http://groups.yahoo.com/group/zertz</a>
+      A newsgroup: 
+      <a href="http://groups.yahoo.com/group/zertz" target="_blank">
+	http://groups.yahoo.com/group/zertz</a>
     </li>
   </ul>
   <h2> Other Zertz implementations </h2>
   <ul>
     <li>
       ZF1: 
-      <a href="http://home.t-online.de/home/320018940973/assimilate.de/ZF1/index.html">
+      <a href="http://home.t-online.de/home/320018940973/assimilate.de/ZF1/index.html" target="_blank">
 	http://home.t-online.de/home/320018940973/assimilate.de/ZF1/index.html</a>
     </li>
     <li>
       MKZertz: 
-      <a href="http://www.mklabs.de/MKZertzDownload/MKZertz.zip">
+      <a href="http://www.mklabs.de/MKZertzDownload/MKZertz.zip" target="_blank">
 	http://www.mklabs.de/MKZertzDownload/MKZertz.zip</a>
     </li>
   </ul>
