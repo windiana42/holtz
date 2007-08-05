7f45b0bc58c295469c75598d396c0471781a0371
Windiana42 <windiana@users.sf.net>
2007-08-05 00:39:39 +0000

    create 3 windows distributions

--- holtz/dist_win.sh
+++ holtz/dist_win.sh
@@ -3,7 +3,10 @@ DIR=holtz-win32;
 BUILDS="msvc cygwin mingw"
 
 for i in $BUILDS; do
-  cp holtz-${i}.exe $DIR/;
+  echo Building zip-package for $i;
+  cp holtz-${i}.exe $DIR/holtz.exe;
+  if cd holtz-win32-${i}; then for j in *; do if test -f $j; then cp $j ../$DIR/$j; fi; done; cd -; fi;
   zip -r holtz-win32-${i}.zip $DIR/holtz.exe $DIR/AUTHORS $DIR/COPYING $DIR/skins/*.zip \
 	$DIR/help/help_*.zip $DIR/sounds/*.wav $DIR/locale/*/LC_MESSAGES/*.mo;
+  if cd holtz-win32-${i}; then for j in *; do if test -f $j; then rm ../$DIR/$j; fi; done; cd -; fi;
 done
