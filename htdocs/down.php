9b93d8e259e5b2db3815c9388243f169a4b3077c
rrobek <florianfischer@gmx.de>
2007-09-03 19:43:12 +0000

    Download Links eingefügt

--- htdocs/down.php
+++ htdocs/down.php
@@ -9,17 +9,17 @@ Note: If you download any source version, you have to download
 tested with wxWidgets 2.8.<br>
 <br>
 Downloads can be found on the <a href="http://sourceforge.net/project/showfiles.php?group_id=74242">
-download page</a> of the holtz project hosted by sourceforge. <br>
+download page</a> of the Holtz project hosted by sourceforge. <br>
 <br>
 Available formats:
 <ul>
-<li>Windows binary (holtz-win32.zip) - just extract and run</li>
-<li>Linux binary package (holtz-1.2.0-1.rpm) - compiled with OpenSuse 10.2, wxWidgets is statically linked</li>
-<li>RPM source package (holtz-1.2.0-1.src.rpm) - to compile a package for your RPM distribution, requires: wxWidgets 2.8</li>
-<li>Source (holtz-1.2.0.tar.gz) - source prepared with autotools, requires: wxWidgets 2.8</li>
+<li>Windows binary (<a href="http://downloads.sourceforge.net/holtz/holtz-win32.zip">holtz-win32.zip</a>) - just extract and run</li>
+<li>Linux binary package (<a href="http://downloads.sourceforge.net/holtz/holtz-1.2.0-1.i586.rpm">holtz-1.2.0-1.rpm</a>) - compiled with OpenSuse 10.2, wxWidgets is statically linked</li>
+<li>RPM source package (<a href="http://downloads.sourceforge.net/holtz/holtz-1.2.0-1.src.rpm">holtz-1.2.0-1.src.rpm</a>) - to compile a package for your RPM distribution, requires: wxWidgets 2.8</li>
+<li>Source (<a href="http://downloads.sourceforge.net/holtz/holtz-1.2.0.tar.gz">holtz-1.2.0.tar.gz</a>) - source prepared with autotools, requires: wxWidgets 2.8</li>
 </ul>
 <br>
-Descriptions of reproducable bugs and compiled packages for other distributions/Operation 
+Descriptions of reproducible bugs and compiled packages for other distributions / operating 
 Systems are always welcome.
  
 <?php EndPage(); ?>
