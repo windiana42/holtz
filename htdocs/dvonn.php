c8eff7ec1ede7f3dfedb1477968eb09f67352d33
Windiana42 <windiana@users.sf.net>
2007-09-05 23:08:31 +0000

    fix tag

--- htdocs/dvonn.php
+++ htdocs/dvonn.php
@@ -12,14 +12,14 @@ The goal is to control as many pieces as possible by stacking them on top of eac
               other and to try to keep your stacks linked to the red DVONN pieces. 
 <h3>Moves</h3>
 <ol>
-  <li><b>Placing pieces</b></li>
+  <li><b>Placing pieces</b>
   <ul>
     <li>The game starts with an empty board. The players place pieces alternatingly, one at a time. <span class="basic">They must start with the DVONN pieces and then continue with their 
               own color.</span></li>
     <li><span class="basic">Continue until all spaces on the board are occupied.<br>
       </span></li>
-  </ul>
-  <li><b>Stacking pieces</b></li>
+  </ul></li>
+  <li><b>Stacking pieces</b>
   <ul>
     <li>The players move alternatingly. Each turn a player must move one piece or one stack. He may only 
               move a piece or a stack of his own color. The color of the topmost piece 
@@ -29,8 +29,8 @@ The goal is to control as many pieces as possible by stacking them on top of eac
     <li>A move may never end in an empty space, but you can move across empty spaces.</li>
     <li>Only pieces or stacks that are not surrounded on all 6 sides can be moved.&nbsp;</li>
     <li>Single DVONN pieces can not be moved, but stacks containing a DVONN piece can. </li>
-  </ul>
-  <li><b>Losing pieces</b></li>
+  </ul></li>
+  <li><b>Losing pieces</b>
   <ul>
     <li>Pieces and stacks must somehow remain in contact with at least 
               one DVONN piece to remain in play. "In contact" means 
@@ -39,13 +39,13 @@ The goal is to control as many pieces as possible by stacking them on top of eac
     <li>All 3 DVONN pieces remain in play until the end of the game, 
               even if one of them becomes isolated, as it will always remain in 
               contact with itself.</li>
-  </ul>
-  <li><b>The End</b></li>
+  </ul></li>
+  <li><b>The End</b>
   <ul>
     <li>The players must play as for long as they can. If one player 
               can't make a move any more, the other must still continue while he can.</li>
     <li>The player who has captured the most pieces in all of his stacks (color does not matter) wins.&nbsp;</li>
-  </ul>
+  </ul></li>
 </ol>
 
 <?php EndPage(1, "dvonn.jpg", "dvonn-board-big.png"); ?>
--- htdocs/helpprog.php
+++ htdocs/helpprog.php
@@ -3,21 +3,21 @@
 ?>
 
   <p>Z&egrave;rtz screen:</p>
-  <img src="scr_shot_zertz_01.jpg"><br>
+  <img src="scr_shot_zertz_01.jpg" alt="screenshot1"><br>
   <p>Dvonn screen:</p>
-  <img src="scr_shot_dvonn_01.jpg"><br>
+  <img src="scr_shot_dvonn_01.jpg" alt="screenshot2"><br>
   <p>Stacking phase with help mode &quot;show possible moves&quot; activated:</p>
-  <img src="scr_shot_dvonn_02.jpg"><br>
+  <img src="scr_shot_dvonn_02.jpg" alt="screenshot3"><br>
   <p>The graphics of Holtz can be modified using skin files. The green and red dots 
   mark a hint move given by the AI.</p>
-  <img src="scr_shot_zertz_02.jpg"><br>
+  <img src="scr_shot_zertz_02.jpg" alt="screenshot4"><br>
   <p>The Z&egrave;rtz game is highly configurable:</p>
-  <img src="scr_shot_game_setup_04.jpg"><br>
+  <img src="scr_shot_game_setup_04.jpg" alt="screenshot5"><br>
   <p>Play Z&egrave;rtz or Dvonn over the network:</p>
-  <img src="scr_shot_network.jpg"><br>
+  <img src="scr_shot_network.jpg" alt="screenshot6"><br>
   <p>For Z&egrave;rtz the layout of board and marbles is configurable:</p>
-  <img src="scr_shot_settings_01.jpg"><br>
+  <img src="scr_shot_settings_01.jpg" alt="screenshot7"><br>
   <p>Z&egrave;rtz and Dvonn offer the choice of skins, move reminder sounds, and fonts:</p>
-  <img src="scr_shot_settings_02.jpg"><br>
+  <img src="scr_shot_settings_02.jpg" alt="screenshot8"><br>
 
 <?php EndPage(0); ?>
