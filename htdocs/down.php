b730930192a4adbd08bd4435eab18814b31c839a
Windiana <windiana@users.sf.net>
2007-12-16 20:44:23 +0000

    homepage update

--- htdocs/down.php
+++ htdocs/down.php
@@ -4,37 +4,42 @@
 
 <h2>Download the Holtz program (source or binary)</h2>
 
-Note: If you download any source version, you have to download 
-<a href="http://wxwidgets.org">wxWidgets</a> first. The source is only 
-tested with wxWidgets 2.8.<br>
 <br>
 <h3>Binary Packages:</h3>
 <ul>
 <li>Windows binary (<a href="http://downloads.sourceforge.net/holtz/holtz-1.2.2-win32.zip">holtz-1.2.2-win32.zip</a>) - just extract and run</li>
 <li>Linux RPM, OpenSUSE 10.3 
-(<a href="http://downloads.sourceforge.net/holtz/holtz-1.2.2-2.i586.rpm">holtz-1.2.2-2.rpm</a>)</li>
+(<a href="http://downloads.sourceforge.net/holtz/holtz-1.2.2-1.i586.rpm">holtz-1.2.2-1.rpm</a>)</li>
 </ul>
 
 <h3>Sourcecode:</h3>
+<p>Note: For compiling the sourcecode you either need to install a development package 
+of wxWidgets (e.g. wxGTK-devel/libwxgtk2.6-dev/libwxgtk2.8-dev) or download source directly 
+from the <a href="http://wxwidgets.org">wxWidgets</a> homepage. Holtz was only 
+tested with wxWidgets 2.8. When using "./configure --enable-wx_2_6" the source should be 
+compatible with wxWidgets 2.6 as well.</p>
 <ul>
-<li>Source (<a href="http://downloads.sourceforge.net/holtz/holtz-1.2.2.tar.gz">holtz-1.2.2.tar.gz</a>) - source prepared with autotools, requires: wxWidgets 2.8</li>
-<li>RPM source package (<a href="http://downloads.sourceforge.net/holtz/holtz-1.2.2-2.src.rpm">holtz-1.2.2-2.src.rpm</a>) - to compile a package for your RPM distribution, requires: wxWidgets 2.8</li>
+<li>Source (<a href="http://downloads.sourceforge.net/holtz/holtz-1.2.2.tar.gz">holtz-1.2.2.tar.gz</a>) - source prepared with autotools, requires: wxWidgets 2.8 or 2.6</li>
+<li>RPM source package (<a href="http://downloads.sourceforge.net/holtz/holtz-1.2.2-1.src.rpm">holtz-1.2.2-1.src.rpm</a>) - to compile a package for your RPM distribution, requires: wxWidgets 2.8</li>
 </ul>
 
-<h3>Untested binary packages created with <a href="http://build.opensuse.org/">
-opensuse build service</a>:</h3>
+<h3>Untested binary packages:</h3>
+<p>Binary packages for Fedora, Mandriva, and other SUSE distributions can be found at 
+<a href="http://software.opensuse.org/search?p=1&q=holtz&baseproject=ALL">opensuse build service</a>.
+Please <a href="mailto:windiana@users.sf.net">report</a> your experiences when 
+using one of these packages so more binary linux packages can be provided on this page.
+</p>
 
-
-<br>
-Older versions can be found on the 
+<h3>Older versions:</h3>
+<p>Older versions can be found on the 
 <a href="http://sourceforge.net/project/showfiles.php?group_id=74242">
-download page</a> of the Holtz project hosted by sourceforge. <br>
-<br>
-Descriptions of reproducible bugs and compiled packages for other distributions / operating 
+download page</a> of the Holtz project hosted by sourceforge.</p>
+
+<h3>Feedback:</h3>
+<p>Descriptions of reproducible bugs and compiled packages for other distributions / operating 
 Systems are always welcome. I am desperately looking for someone with knowlege about Debian 
 and Ubuntu packaging. Please email to <a href="mailto:windiana@users.sf.net">
-windiana@users.sf.net</a>.<br>
-<br>
+windiana@users.sf.net</a>.</p>
 
 <h2>Changes:</h2>
 <h3>1.2.2:</h3>
